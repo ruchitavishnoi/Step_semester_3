b348e154f9f43e0739fd0b264f39284825d8bb46
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-08-29 02:20:19 +0530

    Create LibraryMember.java

---
+++ src/main/java/OOP/assigment_problems/LibraryMember.java
@@ -0,0 +1,53 @@
+class BrokenLibraryMember {
+    static String name;
+    static String memberId;
+    static int booksIssued;
+
+    public BrokenLibraryMember(String name, String memberId, int booksIssued) {
+        BrokenLibraryMember.name = name;
+        BrokenLibraryMember.memberId = memberId;
+        BrokenLibraryMember.booksIssued = booksIssued;
+    }
+}
+
+class LibraryMember {
+    String name;
+    String memberId;
+    int booksIssued;
+
+    static String libraryName = "Central Library";
+    static int memberCount = 0;
+
+    public LibraryMember(String name, int booksIssued) {
+        this.name = name;
+        this.booksIssued = booksIssued;
+        memberCount++;
+        this.memberId = "LM-" + (1000 + memberCount);
+    }
+
+    public void printMemberCard() {
+        System.out.println(name + " | " + memberId);
+    }
+
+    public static void printTotalMembers() {
+        System.out.println("Total members: " + memberCount);
+    }
+}
+
+public class LibraryMember {
+    public static void main(String[] args) {
+        BrokenLibraryMember b1 = new BrokenLibraryMember("Aditi", "LM-1001", 3);
+        BrokenLibraryMember b2 = new BrokenLibraryMember("Rohan", "LM-1002", 1);
+
+        System.out.println(BrokenLibraryMember.name);
+        System.out.println(BrokenLibraryMember.name);
+        System.out.println("(Aditi's data was overwritten \u2013 both members now show \"Rohan\")\n");
+
+        LibraryMember m1 = new LibraryMember("Aditi", 3);
+        LibraryMember m2 = new LibraryMember("Rohan", 1);
+
+        m1.printMemberCard();
+        m2.printMemberCard();
+        LibraryMember.printTotalMembers();
+    }
+}
