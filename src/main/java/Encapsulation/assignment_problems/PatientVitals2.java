b7d10f08ebe3578b23a9e7df5f472e449e8bc56d
ruchitavishnoi <vishnoiruchita@gmail.com>
2026-09-19 00:55:35 +0530

    Create PatientVitals2.java

---
+++ src/main/java/Encapsulation/assignment_problems/PatientVitals2.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+class PatientVitals2 {
+    private double[] readings = new double[500];
+    private int size = 0;
+
+    public PatientVitals2(double[] initialReadings) {
+        if (initialReadings != null) {
+            for (double reading : initialReadings) {
+                recordReading(reading);
+            }
+        }
+    }
+
+    public void recordReading(double reading) {
+        if (reading <= 0 || reading > 45.0) {
+            return;
+        }
+        if (size < readings.length) {
+            readings[size++] = reading;
+        }
+    }
+
+    public double getAverage() {
+        if (size == 0) {
+            return 0.0;
+        }
+        double sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += readings[i];
+        }
+        return sum / size;
+    }
+
+    public double[] getAllReadings() {
+        return Arrays.copyOf(readings, size);
+    }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+        PatientVitals2 v = new PatientVitals2(new double[]{36.5, -2, 37.1});
+        System.out.println(Arrays.toString(v.getAllReadings()));
+
+        double[] copy = v.getAllReadings();
+        copy[0] = 999;
+        System.out.println(v.getAllReadings()[0]);
+    }
+}
